b9f430e562f6b365b18634d18370635562c20590
Abdelkhaliq <111701932+NLPAbdelkhaliq@users.noreply.github.com>
2025-04-02 02:43:19 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ pg = st.navigation(
     {
         "Info": [about_page],
         "Projects": [project_1_page, project_2_page],
+         st.sidebar.markdown(" [Abdelkhaliq](https://youtube.com/@codingisfun)"),
          
     }
 )
