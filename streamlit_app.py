5b7b3eb5d34181a00f6259734b5453682d053b1d
Abdelkhaliq <111701932+NLPAbdelkhaliq@users.noreply.github.com>
2025-04-02 02:44:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ pg = st.navigation(
 # --- SHARED ON ALL PAGES ---
 st.logo("assets/codingisfun_logo.png")
 st.sidebar.markdown(" [Thumbnail Image Generator](https://youtube.com/@codingisfun)")
-st.sidebar.markdown(" [🦀 Rust in Streamlit](https://youtube.com/@codingisfun)")
+st.sidebar.markdown(" [🦀     Rust in Streamlit](https://youtube.com/@codingisfun)")
 st.sidebar.markdown(" [Abdelkhaliq](https://youtube.com/@codingisfun)")
 st.sidebar.markdown(" [Abdelkhaliq](https://youtube.com/@codingisfun)")
 st.sidebar.markdown(" [Abdelkhaliq](https://youtube.com/@codingisfun)")
